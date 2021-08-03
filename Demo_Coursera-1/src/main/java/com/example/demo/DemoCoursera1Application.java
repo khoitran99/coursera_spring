34a41d95be0d5e4a8db4e3c0447a48af4c733c61
khoitran <khoitvhe130007@gmail.com>
2021-08-03 13:13:30 +0700

    first commit

---
+++ Demo_Coursera-1/src/main/java/com/example/demo/DemoCoursera1Application.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import java.util.Arrays;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoCoursera1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCoursera1Application.class, args);
+	}
+
+
+
+}
